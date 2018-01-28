f3b363e27ab30dee8e40fa438837a7c9eafedccb
Cloud User <ec2-user@ip-172-31-16-183.us-west-2.compute.internal>
2018-01-28 19:51:04 +0000

    ddlscrpit created

---
+++ ddl.sql
@@ -0,0 +1,9 @@
+create database  IF NOT EXISTS amax;
+use amax;
+create table IF NOT EXISTS hrtraining (
+	esd varchar(255),
+	isoprocedure varchar(255),
+	depttraining varchar(255),
+	city varchar(255)
+
+);
